9eee0df3d46b258c9893dfc36eb3d17de5986940
zhangyide <zhangyide@9fbank.cc>
2018-11-13 17:36:28 +0800

    大方的身份

--- KuangJia/KuangJia/CustomService/viewTools/UtinityDefine.h
+++ KuangJia/KuangJia/CustomService/viewTools/UtinityDefine.h
@@ -31,18 +31,6 @@
 #define IOS8DEVICE ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
 
 
-#define HttpHeader @"http://124.67.165.208:1980/mzl/app/"
-//高德POI点搜索请求头
-#define HttpGaoDePOIHeader @"http://restapi.amap.com/v3/place/"
-
-#define GaoDeSearchPOIKey  @"8fffb6893653a7f0768c264d16c150fa"
-#define GaoDeIOSSDKKey @"c89c62ac7dd195875cda7b787e74d4f6"
-
-#define BaiDuVoiceAppID @"9617216"
-#define BaiDuVoiceAPI_KEY @"P1zoy3oAqbDGaqiqqMp2fq70" // 请修改为您在百度开发者平台申请的API_KEY
-#define BaiDuVoiceSECRET_KEY @"e5f49fa4662c1b1fe3fd5fe9e3833691" // 请修改您在百度开发者平台申请的SECRET_KEY
-
-
 
 //#define ARTADEBUG
 //宏输出函数
