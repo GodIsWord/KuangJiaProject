fe1f8acff03e918542a55120e4f21bb0fd26e53b
zhangyide <zhangyide@9fbank.cc>
2018-11-20 19:35:01 +0800

    我的页面修改

--- KuangJia/KuangJia/MineViewController.m
+++ KuangJia/KuangJia/MineViewController.m
@@ -10,6 +10,8 @@
 
 #import "HomeSecriteViewController.h"
 
+#import "KJLoginManage.h"
+
 @interface MineViewController ()<UITableViewDelegate,UITableViewDataSource>
 
 @property(nonatomic,strong) UITableView *tableView;
@@ -23,19 +25,58 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.title = @"个人信息";
-    self.dataSource = @[@"功能演示",@"修改密码",@"退出登录"];
+    
+    self.dataSource = @[@"功能演示",@"修改密码"];
     [self createTableView];
 }
 
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+-(void) reloadView{
+    [self.tableView reloadData];
+}
 -(void)createTableView
 {
     UITableView *tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height) style:UITableViewStylePlain];
     tableView.delegate = self;
     tableView.dataSource = self;
+    tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
     [self.view addSubview:tableView];
     self.tableView = tableView;
 }
 
+-(CGFloat) tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
+{
+    NSUserDefaults *sidDefaults = [NSUserDefaults standardUserDefaults];
+    NSString *sid = [sidDefaults valueForKey:@"sid"];
+    if (sid.length>0) {
+        return 60;
+    }
+    return 0;
+}
+-(UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section
+{
+    NSUserDefaults *sidDefaults = [NSUserDefaults standardUserDefaults];
+    NSString *sid = [sidDefaults valueForKey:@"sid"];
+    if (sid.length>0) {
+        UIView *backView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 60)];
+        backView.backgroundColor = [UIColor whiteColor];
+        backView.userInteractionEnabled = YES;
+        
+        UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+        btn.frame = CGRectMake(0, 40, backView.bounds.size.width, 20);
+        [btn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+        [btn setTitle:@"退出登录" forState:UIControlStateNormal];
+        [btn addTarget:self action:@selector(logOut) forControlEvents:UIControlEventTouchUpInside];
+        [backView addSubview:btn];
+        
+        return backView;
+    }
+    return nil;
+    
+}
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     return self.dataSource.count;
 }
@@ -59,15 +100,37 @@
             
         }
             break;
-        case 2:
-        {
-            
-        }
-            break;
             
         default:
             break;
     }
 }
-
+-(void)logOut{
+    [KJLoginManage exitsuccess:^(NSDictionary *result) {
+        if ([result[@"result"] isEqualToString:@"ok"]) {
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"提示信息"
+                                                            message:@"注销成功"
+                                                           delegate:nil
+                                                  cancelButtonTitle:@"确定"
+                                                  otherButtonTitles:nil, nil];
+            [alert show];
+            [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"sid"];
+            [self reloadView];
+        }else{
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"提示信息"
+                                                            message:result[@"msg"]
+                                                           delegate:nil
+                                                  cancelButtonTitle:@"确定"
+                                                  otherButtonTitles:nil, nil];
+            [alert show];
+        }
+    } fail:^(NSError *error) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"提示信息"
+                                                        message:error.localizedDescription
+                                                       delegate:nil
+                                              cancelButtonTitle:@"确定"
+                                              otherButtonTitles:nil, nil];
+        [alert show];
+    }];
+}
 @end
